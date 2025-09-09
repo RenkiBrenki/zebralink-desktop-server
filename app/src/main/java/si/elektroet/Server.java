cf5632558d3b6bedf8c7b796fc8174d4ba4a66ab
Rene Jausovec <jausovec.rene@gmail.com>
2025-09-09 17:09:25 +0200

    added install script

--- app/src/main/java/si/elektroet/Server.java
+++ app/src/main/java/si/elektroet/Server.java
@@ -27,6 +27,7 @@ class Server implements AutoCloseable {
 
     private static String SUCCESS_MESSAGE = "Data received successfully";
 
+	
     public Server(Logger logger) throws IOException, AWTException {
         serverSocket = new ServerSocket(PORT);
         this.typeUtil = new TypeUtil();
